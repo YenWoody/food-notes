053350780867826a354bde2c59fa7da6421fd18e
YenWoody <yenph@fimo.edu.vn>
2025-07-21 22:09:34 +0700

    update title

--- app.vue
+++ app.vue
@@ -24,4 +24,23 @@
   scale: 0.95;
 }
 </style>
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+import { useHead } from '#imports'
+
+const route = useRoute()
+
+useHead(() => {
+  const titleMap = {
+    '/': 'Trang chủ Nhăm Nhăm',
+    '/login': 'Đăng nhập Nhăm Nhăm',
+    '/register': 'Đăng ký Nhăm Nhăm',
+    '/add-note': 'Thêm món đã nhăm nhăm nào!',
+    '/map': 'Bản đồ các quán đã nhăm nhăm!'
+  }
+
+  return {
+    title: titleMap[route.path] || 'Ứng dụng ghi chú Nhăm Nhăm'
+  }
+})
+</script>
